e80724af96edc3a0e3ec2de589ff516e7b00c4bd
Max Foxxo <littlenicefox@mail.ru>
2018-11-30 22:02:54 +0300

    Check answers

--- app/src/main/java/com/example/foxxo/eduproject/TestActivity.java
+++ app/src/main/java/com/example/foxxo/eduproject/TestActivity.java
@@ -1,6 +1,7 @@
 package com.example.foxxo.eduproject;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import java.io.IOException;
 import java.io.InputStream;
@@ -9,6 +10,8 @@ import java.util.HashMap;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
+
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.*;
 import android.view.*;
@@ -19,6 +22,8 @@ public class TestActivity extends Activity {
     LinearLayout llTest;
 
     Button btnCheck;
+    TextView infoText;
+
 
     ArrayList<String> answerSelects = new ArrayList<String>();
 
@@ -47,7 +52,6 @@ public class TestActivity extends Activity {
         lParams.gravity = Gravity.LEFT;
         for (int i = 0; i < questions.size(); i++) {
             item = new LinearLayout(this);
-            item.setBackgroundColor(Integer.parseInt("00E0E0", 16));
             item.setOrientation(LinearLayout.VERTICAL);
             TextView textView = new TextView(this);
             textView.setTag(i);
@@ -67,6 +71,11 @@ public class TestActivity extends Activity {
             llTest.addView(item);
         }
 
+        infoText = new TextView(this);
+        infoText.setText("");
+        infoText.setVisibility(View.INVISIBLE);
+        llTest.addView(infoText);
+
         btnCheck = new Button(this);
         btnCheck.setText("Проверить");
         llTest.addView(btnCheck);
@@ -74,7 +83,28 @@ public class TestActivity extends Activity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //View radioButton = radioButtonGroup.findViewById(radioButtonID);
+                ArrayList<Integer> wrongAnswers = new ArrayList<Integer>();
+                int count = answers.size();
+                for (int i = 0; i < count; i++) {
+                    LinearLayout item = (LinearLayout)llTest.getChildAt(i);
+                    RadioGroup group = (RadioGroup)item.getChildAt(1);
+                    int id = group.getCheckedRadioButtonId();
+                    View radioButton = group.findViewById(id);
+                    int idx = group.indexOfChild(radioButton);
+                    if (idx != answers.get(i)) {
+                        wrongAnswers.add(i+1);
+                    }
+                }
+                if (wrongAnswers.size() == 0) {
+                    infoText.setText("Все ответы правильные!");
+                    infoText.setTextColor(Color.GREEN);
+                    infoText.setVisibility(View.VISIBLE);
+                } else {
+                    String errStr = TextUtils.join(" ", wrongAnswers);
+                    infoText.setText("Неправильные ответы: " + errStr);
+                    infoText.setTextColor(Color.RED);
+                    infoText.setVisibility(View.VISIBLE);
+                }
             }
         });
     }
